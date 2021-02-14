f15aa563669e7181805ae7172d0b0ad1309ba500
Sanjay krishna anbalagan <sanjay.krishna@cengage.com>
2021-02-14 08:52:31 +0530

    Theme B changes added

--- src/header/index.js
+++ src/header/index.js
@@ -1,31 +1,28 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React from 'react';
 import NavBar from 'abstract/ui/navbar';
 import Logo from './logo';
 
 export default function Header(props){
-	const {links, logoUrl} = props;
+  const {links, logoUrl} = props;
 
 
-	const style = {
-		display: 'flex',
-		marginRight: '16px',
-		marginLeft: '32px'
-	};
+  const style = {
+    display: 'flex'
+  };
 
-	const wordStyle = {
-		width: "50px"
-	};
+  const wordStyle = {
+    width: "50px"
+  }
 
-	return (
-		<div className='header'>
-			<div style={style}>
-				<Logo url={logoUrl}/>
-			</div>
-			<div>
-				<h3 style={wordStyle}>Afro Caribbean Community Association</h3>
-			</div>
-			<NavBar links={links}/>
-		</div>
-	)
+  return (
+  <div className='header'>
+    <div style={style}>
+      <Logo url={logoUrl}/>
+    </div>
+    <div>
+      <h2 style={wordStyle}>Afro Caribbean Community Association</h2>
+    </div>
+    <NavBar links={links}/>
+  </div>
+  )
 }
-
--- src/header/logo.js
+++ src/header/logo.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 export default function Logo(props){
-	const {url} = props;
+  const {url} = props;
 
-	const containerStyle = {
-		position: 'relative'
-	};
+  const containerStyle = {
+    position: 'relative',
+    top: '24px'
+  };
 
-	return (
-		<div style={containerStyle}>
-			<div className='logo'>
-				<img src={url}/>
-			</div>
-		</div>
-	)
+  return (
+  <div style={containerStyle}>
+    <div className='logo'>
+      <img src={url}/>
+    </div>
+  </div>
+  )
 }
-
